9e7fe0fb8b3404093a6ba1b00d7a3c34138c9fb3
Boris <boris_galunga@mail.ru>
2022-11-22 13:56:35 +0300

    Reducer. Пример кода, как она работает под капотом

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+let reducer = (state = 0, action) => {
+	if (state == undefined) {
+		return 0;
+	}
+	switch (action.type) {
+		case "INC":
+			return state + 1;
+		default:
+			return state;
+	}
+	return 0;
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let state = reducer(undefined, {});
+console.log(state);
+state = reducer(state, { type: "INC" });
+state = reducer(state, { type: "INC" });
+state = reducer(state, { type: "INC" });
+console.log(state);
